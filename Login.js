69a5385a2ac483a998bc8d4f718613d77c6db589
Diego Miranda <diegomo1224@gmail.com>
2023-05-03 01:22:40 -0300

    atualizações dos codigos

--- Login.js
+++ Login.js
@@ -1,32 +1,34 @@
-function main() {
-    var usuario;
-    var senha;
-    var recorrente;
-var i=0; 
-    var musuario=["pedrolongo","pedroh"]; 
-var msenha=["1234","5678"];
-    recorrente = true;
-    do {
-        usuario = window.prompt('Enter a value for usuario'); 
-if (usuario != musuario[i]) {i++} 
-if(musuario==null){i--} 
-        senha = window.prompt('Enter a value for senha');
-        if (usuario == musuario[i]) {
-            if (senha == msenha[i]) {
-
-                window.alert("usuario e senha OK");
-                recorrente = true;
-            }
-        }
-
-        if (usuario == musuario[i]) {
-            if (senha == msenha[i]) {
-
-                window.alert("usuario e senha OK");
-                recorrente = true;
-            }
-        }
-    } while (recorrente == true);
-var i=0; 
-
-} 
+// Definir usuários válidos em um objeto JSON
+const usuariosValidos = {
+    "usuario1": {
+      "cpf": "477.543.498-50",
+      "senha": "senha123"
+    },
+    "usuario2": {
+      "cpf": "222.222.222-22",
+      "senha": "senha456"
+    },
+    "usuario3": {
+      "cpf": "333.333.333-33",
+      "senha": "senha789"
+    }
+  };
+  
+  // Função de validação de login
+  function validarLogin() {
+    // Obter os valores de entrada do CPF e da senha
+    const cpf = document.querySelector('.cpf').value;
+    const senha = document.querySelector('.senha').value;
+  
+    // Verificar se o CPF e a senha correspondem a um usuário válido
+    for (let usuario in usuariosValidos) {
+      if (usuariosValidos[usuario].cpf === cpf && usuariosValidos[usuario].senha === senha) {
+        alert("Login válido para o usuário " + usuario);
+        return true; // login válido
+      }
+    }
+  
+    alert("CPF ou senha inválido"); // login inválido
+    return false;
+  }
+  
